594d02db4786bd5581a3f638f609e86d2073ab4e
Gautier <gautier.dumonteil@gmail.com>
2010-06-04 23:33:11 -0400

    fixed wrong bag name setup_single.sh

--- ar_pose/demo/setup_single.sh
+++ ar_pose/demo/setup_single.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget http://robotics.ccny.cuny.edu/data/ROS/usb_cam_topics_ar_single_cam_param.bag.tar.gz
-tar -xzvf usb_cam_topics_ar_single_cam_param.bag.tar.gz 
+wget http://robotics.ccny.cuny.edu/data/ROS/usb_cam_topics_ar_single.bag.tar.gz
+tar -xzvf usb_cam_topics_ar_single.bag.tar.gz 
